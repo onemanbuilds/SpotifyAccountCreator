927beec11ee48723a2893c7a727b18f4b6eee018
onemanbuilds <72015638+onemanbuilds@users.noreply.github.com>
2021-01-08 11:34:15 +0100

    update
    - Added webhook support.
    - Changed printing color.
    - Added webhook retries to the title.
    - Added config.json, you don't need to enter your settings everytime you run the app anymore.
    - Removed some useragents.
    - Changed file orders.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from colorama import init,Style,Fore
 from bs4 import BeautifulSoup
 from threading import Thread,Lock,active_count
 from time import sleep
+from datetime import datetime
 
 class Main:
 
@@ -19,16 +20,25 @@ class Main:
         else:
             print("\n") * 120
 
-    def SetTitle(self,title_name:str):
-        system("title {0}".format(title_name))
+    def SetTitle(self,title:str):
+        if name == 'posix':
+            stdout.write(f"\x1b]2;{title}\x07")
+        elif name in ('ce', 'nt', 'dos'):
+            system(f'title {title}')
+        else:
+            stdout.write(f"\x1b]2;{title}\x07")
 
     def ReadFile(self,filename,method):
         with open(filename,method,encoding='utf8') as f:
             content = [line.strip('\n') for line in f]
             return content
 
+    def ReadJson(self,filename,method):
+        with open(filename,method) as f:
+            return json.load(f)
+
     def GetRandomUserAgent(self):
-        useragents = self.ReadFile('useragents.txt','r')
+        useragents = self.ReadFile('[Data]/useragents.txt','r')
         return choice(useragents)
 
     def PrintText(self,bracket_color:Fore,text_in_bracket_color:Fore,text_in_bracket,text):
@@ -39,7 +49,7 @@ class Main:
         self.lock.release()
 
     def GetRandomProxy(self):
-        proxies_file = self.ReadFile('proxies.txt','r')
+        proxies_file = self.ReadFile('[Data]/proxies.txt','r')
         proxies = {}
         if self.proxy_type == 1:
             proxies = {
@@ -60,11 +70,9 @@ class Main:
 
     def __init__(self):
         init(convert=True)
-        self.createds = 0
-        self.retries = 0
-        self.SetTitle('One Man Builds Spotify Account Creator Tool')
+        self.SetTitle('[One Man Builds Spotify Account Creator Tool]')
         self.clear()
-        self.title = Style.BRIGHT+Fore.RED+"""
+        self.title = Style.BRIGHT+Fore.GREEN+"""
                                    ╔════════════════════════════════════════════════╗ 
                                        ╔═╗╔═╗╔═╗╔╦╗╦╔═╗╦ ╦  ╔═╗╔═╗╔═╗╔═╗╦ ╦╔╗╔╔╦╗
                                        ╚═╗╠═╝║ ║ ║ ║╠╣ ╚╦╝  ╠═╣║  ║  ║ ║║ ║║║║ ║ 
@@ -77,15 +85,22 @@ class Main:
                                 
         """
         print(self.title)
-        self.method = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] ['+Fore.RED+'1'+Fore.CYAN+']Valid Credentails Lookalike (slower) ['+Fore.RED+'0'+Fore.CYAN+']Random Strings (faster): '))
-        self.use_proxy = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] ['+Fore.RED+'1'+Fore.CYAN+']Proxy ['+Fore.RED+'0'+Fore.CYAN+']Proxyless: '))
 
-        if self.use_proxy == 1:
-            self.proxy_type = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] ['+Fore.RED+'1'+Fore.CYAN+']Https ['+Fore.RED+'2'+Fore.CYAN+']Socks4 ['+Fore.RED+'3'+Fore.CYAN+']Socks5: '))
-        
-        self.threads = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Threads: '))
-        self.birth_year_start = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Birth year start: '))
-        self.birth_year_end = int(input(Style.BRIGHT+Fore.CYAN+'['+Fore.RED+'>'+Fore.CYAN+'] Birth year end: '))
+        config = self.ReadJson('[Data]/configs.json','r')
+
+        self.method = config['method']
+        self.use_proxy = config['use_proxy']
+        self.proxy_type = config['proxy_type']
+        self.threads = config['threads']
+        self.birth_year_start = config['birth_year_start']
+        self.birth_year_end = config['birth_year_end']
+        self.webhook_enable = config['webhook_enable']
+        self.webhook_url = config['webhook_url']
+
+        self.createds = 0
+        self.retries = 0
+        self.webhook_retries = 0
+
         self.lock = Lock()
         print('')
 
@@ -146,8 +161,40 @@ class Main:
 
     def TitleUpdate(self):
         while True:
-            self.SetTitle(f'One Man Builds Spotify Account Creator ^| CREATED: {self.createds} ^| RETRIES: {self.retries} ^| THREADS: {active_count()-1}')
+            self.SetTitle(f'[One Man Builds Spotify Account Creator] ^| CREATED: {self.createds} ^| WEBHOOK RETRIES: {self.webhook_retries} ^| RETRIES: {self.retries} ^| THREADS: {active_count()-1}')
             sleep(0.1)
+
+    def SendWebhook(self,title,message,icon_url,thumbnail_url,proxy,useragent):
+        try:
+            timestamp = str(datetime.utcnow())
+
+            message_to_send = {"embeds": [{"title": title,"description": message,"color": 65362,"author": {"name": "AUTHOR'S DISCORD SERVER [CLICK HERE]","url": "https://discord.gg/9bHfzyCjPQ","icon_url": icon_url},"footer": {"text": "MADE BY ONEMANBUILDS","icon_url": icon_url},"thumbnail": {"url": thumbnail_url},"timestamp": timestamp}]}
+            
+            headers = {
+                'User-Agent':useragent,
+                'Pragma':'no-cache',
+                'Accept':'*/*',
+                'Content-Type':'application/json'
+            }
+
+            payload = json.dumps(message_to_send)
+
+            if self.use_proxy == 1:
+                response = requests.post(self.webhook_url,data=payload,headers=headers,proxies=proxy)
+            else:
+                response = requests.post(self.webhook_url,data=payload,headers=headers)
+
+            if response.text == "":
+                pass
+            elif "You are being rate limited." in response.text:
+                self.webhook_retries += 1
+                self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
+            else:
+                self.webhook_retries += 1
+                self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
+        except:
+            self.webhook_retries += 1
+            self.SendWebhook(title,message,icon_url,thumbnail_url,proxy,useragent)
 
     def SpotifyCreator(self):
         try:
@@ -176,9 +223,11 @@ class Main:
             payload = 'creation_point=lite_7e7cf598605d47caba394c628e2735a2&password_repeat={0}&platform=Android-ARM&iagree=true&password={1}&gender={2}&key=a2d4b979dc624757b4fb47de483f3505&birth_day={3}&birth_month={4}&email={5}&birth_year={6}'.format(credentails['password'],credentails['password'],credentails['gender'],credentails['birth_day'],credentails['birth_month'],credentails['email'],credentails['birth_year'])
             
             create_response = ''
+            proxy = ''
 
             if self.use_proxy == 1:
-                create_response = requests.post(create_url, data=payload, headers=create_headers,proxies=self.GetRandomProxy(),timeout=5)
+                proxy = self.GetRandomProxy()
+                create_response = requests.post(create_url, data=payload, headers=create_headers,proxies=proxy,timeout=5)
             else:
                 create_response = requests.post(create_url, data=payload, headers=create_headers)
 
@@ -188,12 +237,14 @@ class Main:
                 if json_data['status'] == 1:
                     username = json_data['username']
                     if username != '':
-                        self.PrintText(Fore.CYAN,Fore.RED,'CREATED','{0}:{1} | {2} | {3} | {4}/{5}/{6}'.format(credentails['email'],credentails['password'],credentails['username'],credentails['gender'],credentails['birth_year'],credentails['birth_month'],credentails['birth_day']))
+                        self.PrintText(Fore.WHITE,Fore.GREEN,'CREATED','{0}:{1} | {2} | {3} | {4}/{5}/{6}'.format(credentails['email'],credentails['password'],credentails['username'],credentails['gender'],credentails['birth_year'],credentails['birth_month'],credentails['birth_day']))
                         self.createds += 1
-                        with open('hits.txt','a') as f:
+                        with open('[Data]/[Results]/hits.txt','a') as f:
                             f.write('{0}:{1}\n'.format(credentails['email'],credentails['password']))
-                        with open('detailed_hits.txt','a') as f:
+                        with open('[Data]/[Results]/detailed_hits.txt','a') as f:
                             f.write('{0}:{1} | {2} | {3} | {4}/{5}/{6}\n'.format(credentails['email'],credentails['password'],credentails['username'],credentails['gender'],credentails['birth_year'],credentails['birth_month'],credentails['birth_day']))
+                        if self.webhook_enable == 1:
+                            self.SendWebhook('Spotify Account','**COMBO**\n{0}:{1}\n**USERNAME**\n{2}\n**GENDER**\n{3}\n**BIRTHDATE**\n{4}/{5}/{6}'.format(credentails['email'],credentails['password'],credentails['username'],credentails['gender'],credentails['birth_year'],credentails['birth_month'],credentails['birth_day']),'https://cdn.discordapp.com/attachments/776819723731206164/796935218166497352/onemanbuilds_new_logo_final.png','https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Spotify_logo_without_text.svg/768px-Spotify_logo_without_text.svg.png',proxy,self.GetRandomUserAgent())
                     else:
                         self.retries += 1
                         self.SpotifyCreator()
